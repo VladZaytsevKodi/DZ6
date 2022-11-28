5ba572d69df59a23957e27bc6b322882dfef0b41
Vlad <vlad_zaycev_1984@mail.ru>
2022-11-28 18:25:17 +0300

    перва задача 43

--- Domashka_6_43/Program.cs
+++ Domashka_6_43/Program.cs
@@ -1,4 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
 Console.WriteLine("введите значение b1");
 double b1 = Convert.ToInt32(Console.ReadLine());
 
